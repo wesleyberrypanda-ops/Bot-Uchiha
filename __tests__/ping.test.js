0a2cb61802ed04110a9d4142ffcdc361a9ef2ded
Rúben Silver <drogadinhosilver@gmail.com>
2025-12-02 06:28:24 +0100

    Delete __tests__/ping.test.js

--- __tests__/ping.test.js
+++
@@ -1,9 +0,0 @@
-const { ping } = require('../ping');
-
-// Teste para a funcionalidade do ping
-describe('Ping module', () => {
-  test('Deve retornar PONG', () => {
-    const resultado = ping();
-    expect(resultado).toBe('PONG');
-  });
-});

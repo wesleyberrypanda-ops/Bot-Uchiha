20fb3d19910386ac6164cdce52eb4668a950228f
Rúben Silver <drogadinhosilver@gmail.com>
2025-12-02 06:37:38 +0100

    Delete showQR.sh

--- showQR.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Script para gerar QR no Termux a partir da string do WhatsApp Bot
-
-echo "Cole aqui a string do QR do seu bot e pressione Enter:"
-read QRSTRING
-
-# Gerar QR no terminal
-qrencode -t ansiutf8 "$QRSTRING"
